174a2d4ee825a8cd7a9a598e3ab321af044a9325
SiberianHusky <3088506834@qq.com>
2025-01-15 23:25:09 +0800

    1.0.2+4 added: 教学评价 fixed: 统一主页阴影风格

---
+++ lib/pages/app/oa/evaluate_online/state.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import 'package:swust_link/common/entity/oa/evaluation_paper.dart';
+import 'package:swust_link/spider/evaluate_online.dart';
+
+class EvaluateOnlineState {
+  final evaluatePaperList = <EvaluationPaper>[].obs;
+  late EvaluateOnline epClient;
+  final isLoading = true.obs;
+}
